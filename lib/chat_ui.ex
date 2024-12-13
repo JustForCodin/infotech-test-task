5cd53e546eef17e7155f9a114a6f0722a590eae1
JustForCodin <musicjohnliofficial@gmail.com>
2024-12-13 17:14:08 +0200

    bug fixes

--- lib/chat_ui.ex
+++ lib/chat_ui.ex
@@ -1,21 +1,22 @@
 defmodule ChatUI do
-  use N2O, with: [:nitro]
+  # use N2O, with: [:nitro]
+  import NITRO
 
   # Головна сторінка
   def index do
-    Nitro.page([
-      Nitro.button(id: "room1", body: "Кімната 1", postback: "room1"),
-      Nitro.button(id: "room2", body: "Кімната 2", postback: "room2"),
-      Nitro.button(id: "room3", body: "Кімната 3", postback: "room3")
+    NITRO.panel([
+      NITRO.button(id: "room1", body: "Кімната 1", postback: "room1"),
+      NITRO.button(id: "room2", body: "Кімната 2", postback: "room2"),
+      NITRO.button(id: "room3", body: "Кімната 3", postback: "room3")
     ])
   end
 
   # Сторінка кімнати
   def room(room) do
-    Nitro.page([
-      Nitro.input(id: "message_input", placeholder: "Введіть повідомлення"),
-      Nitro.button(id: "send_button", body: "Надіслати", postback: "send"),
-      Nitro.div(id: "messages", body: "Повідомлення тут з'являтимуться")
+    NITRO.panel([
+      NITRO.input(id: "message_input", placeholder: "Введіть повідомлення"),
+      NITRO.button(id: "send_button", body: "Надіслати", postback: "send"),
+      NITRO.div(id: "messages", body: "Повідомлення тут з'являтимуться")
     ])
   end
 end
